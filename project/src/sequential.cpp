c222667a704d40703f2904b17989da864a74a060
educscs <109014596+educscs@users.noreply.github.com>
2022-09-14 01:35:44 -0400

    fixed errors

--- project/src/sequential.cpp
+++ project/src/sequential.cpp
@@ -1,44 +1,63 @@
-#include "sequential.hpp"
-
-
-#include <climits>
-#include <cstring>
+#include <bits/stdc++.h>
 #include <fstream>
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
-
-// Numero maximo de registros en el aux
-#define K 10;
 
 struct Record {
   string Name;
   double Earnings;
   int Year;
   string Sport;
-  int nextDel;
 };
 
-ostream &operator<<(ostream &stream, Record &record) {
-  stream.write(record.nombre, sizeof(record.nombre));
-  stream.write(record.codigo, sizeof(record.codigo));
-  stream.write(record.apellidos, sizeof(record.apellidos));
-  stream.write(record.carrera, sizeof(record.carrera));
-  stream << "\n";
-  stream << flush;
-  return stream;
+struct DataRecord {
+double Earnings;
+int pos;
+int next;
+//int file; // 0->datos.dat  1->auxiliar.dat
+};
+
+void init(){
+  ifstream file("forbesathletesv2.csv",ios::binary );
+  ofstream file2;
+  file2.open("datos.dat",ios::binary);
+  string line;
+  int pos = 0;
+  int siguiente = 0;
+  int archivo = 0;
+  while(getline(file,line)){
+    Record record;
+    char *data = const_cast<char *>(line.c_str());
+    record.Name = strtok(data, ",");
+    record.Earnings = stod(strtok(NULL, ","));
+    record.Year = stoi(strtok(NULL, ","));
+    record.Sport = strtok(NULL, "\r");
+    siguiente = file.tellg();
+    file2.write((char*)&(record.Earnings), sizeof(double));
+    file2.write((char*)&pos, sizeof(int));
+    file2.write((char*)&siguiente, sizeof(int));
+    //file2.write((char*)&archivo, sizeof(int));
+    pos=siguiente;
+  }
+  file2.close();
 }
-istream &operator>>(istream &stream, Record &record) {
-  stream.read(record.nombre, sizeof(record.nombre));
-  stream.read(record.codigo, sizeof(record.codigo));
-  stream.read(record.apellidos, sizeof(record.apellidos));
-  stream.read(record.carrera, sizeof(record.carrera));
-  stream.get();
-  record.codigo[4] = '\0';
-  record.nombre[10] = '\0';
-  record.apellidos[19] = '\0';
-  record.carrera[14] = '\0';
-  return stream;
+
+void readFile(){
+  ifstream file2("datos.dat",ios::binary);
+
+while(file2.peek() != EOF){
+  DataRecord record;
+  file2.read((char*)&record,sizeof(DataRecord));
+  cout<<record.Earnings<<" "<<record.pos<<" "<<record.next<<endl;
+}
+}
+DataRecord readRecord(int pos){
+  ifstream file2("datos.dat",ios::binary);
+  file2.seekg(pos*sizeof(DataRecord),ios::beg);
+  DataRecord record;
+  file2.read((char*)&record,sizeof(DataRecord));
+  return record;
 }
 
 class SequentialFile {
@@ -49,46 +68,33 @@ class SequentialFile {
 
 public:
   SequentialFile(string fileName) : fileName(fileName) {}
-  vector<Record> search(int key) {
+  vector<DataRecord> search(double key) {
+    vector<DataRecord> ans;
     fstream file;
-    file.open(fileName, ios::out);
+    file.open(fileName, ios::binary|ios::ate|ios::app );
     int l = 0;
-    int u = file.tellp();
-    while (l <= u) {
-      int mid = l + (u - l) / 2;
-      Record re = readRecord(mid);
-      if (re.Year == key) {
-        return mid;
-      } else if (re.year < key) {
-        l = mid + 1;
-      } else {
+    int r = file.tellp()/sizeof(DataRecord) - 1;
+    cout<<r<<endl;
+    while (l <= r) {
+      int mid = l + (r - l) / 2;
+      DataRecord re = readRecord(mid);
+      cout<<re.Earnings<<endl;
+      if (re.Earnings == key) {
+        ans.push_back(re);
+        return ans;
+      } else if (re.Earnings < key) {
         r = mid - 1;
+      } else {
+        l = mid + 1;
       }
-      return mid;
     }
-  }
-
-  Record readRecord(int pos) {
-    ifstream File(this->fileName, ios::in);
-    if (!File.is_open())
-      cout << "No se pudo abrir el archivo. \n";
-    string line;
-    getline(File, line);
-    for (int i = 0; i < pos; ++i)
-      getline(File, line);
-    Record record;
-    char *data = const_cast<char *>(line.c_str());
-    record.Name = strtok(data, ",");
-    record.Earnings = strtok(NULL, ",");
-    record.Year = strtok(NULL, ",");
-    record.Sport = atoi(strtok(NULL, ","));
-    return record;
+    return ans;
   }
 
   bool add(Record registro) {
-    
+
   }
-  bool remove(T key) {}
+  bool remove(int key) {}
 
   //******Funciones auxiliares*****
   // Retorna el numero de registros del aux
@@ -97,8 +103,11 @@ public:
   // reconstruye los archivos cuando sizeAux llega a K
   void reconstruction() { return 0; }
 };
-
-int main() { std::cout << "Hello World!\n"; }
-
-// https://www.kaggle.com/datasets/darinhawley/forbes-high-paid-athletes-19902021
+int main() {
+  init();
+  SequentialFile sf("datos.dat");
+  auto ans=sf.search(130);
+  cout<<ans.size()<<endl;
+  }
+/*https://www.kaggle.com/datasets/darinhawley/forbes-high-paid-athletes-19902021
 */
