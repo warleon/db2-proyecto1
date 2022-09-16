2963467a63c0bce537f481c2c6c630227539ab3e
warleon <51892390+warleon@users.noreply.github.com>
2022-09-16 10:59:07 -0500

    add hastable psudocode

--- project/tests/hashTableTest.cpp
+++ project/tests/hashTableTest.cpp
@@ -2,18 +2,12 @@
 
 #include "hashTable.hpp"
 
+TEST(HashTable, constructorTest_0) {}
+
 TEST(HashTable, searchTest_0) {}
-TEST(HashTable, searchTest_1) {}
-TEST(HashTable, searchTest_2) {}
 
 TEST(HashTable, rangeSearchTest_0) {}
-TEST(HashTable, rangeSearchTest_1) {}
-TEST(HashTable, rangeSearchTest_2) {}
 
 TEST(HashTable, addTest_0) {}
-TEST(HashTable, addTest_1) {}
-TEST(HashTable, addTest_2) {}
 
 TEST(HashTable, removeTest_0) {}
-TEST(HashTable, removeTest_1) {}
-TEST(HashTable, removeTest_2) {}
