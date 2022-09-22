20b7e501e33eafb7f5d1cf3a84159c177c95418e
warleon <51892390+warleon@users.noreply.github.com>
2022-09-22 05:38:59 -0500

    sufrir en crow

--- project/demo/main.cpp
+++ project/demo/main.cpp
@@ -1,12 +1,15 @@
-
+#define CROW_STATIC_DIRECTORY "/static/"
 #include <crow_all.h>
 
+#include <filesystem>
+#include <hashTable.hpp>
 #include <iostream>
 
 int main(int argc, char** argv) {
   crow::SimpleApp app;
 
-  CROW_ROUTE(app, "/")([]() { return "Hello world"; });
+  CROW_ROUTE(app, "/")
+  ([]() { return "hola"; });
 
   app.port(80).multithreaded().run();
   return 0;
