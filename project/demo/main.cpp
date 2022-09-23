86cb95a92a9afea501a7c33b707054fec49bd1be
warleon <51892390+warleon@users.noreply.github.com>
2022-09-22 20:13:46 -0500

    cmake fails dont know why

--- project/demo/main.cpp
+++ project/demo/main.cpp
@@ -11,6 +11,19 @@ int main(int argc, char** argv) {
   CROW_ROUTE(app, "/")
   ([]() { return "hola"; });
 
+  CROW_ROUTE(app, "/add/").methods("POST"_method)([](const crow::request& req) {
+    return req.body;
+  });
+  CROW_ROUTE(app, "/search/")
+      .methods("POST"_method)(
+          [](const crow::request& req) { return req.body; });
+  CROW_ROUTE(app, "/delete/")
+      .methods("POST"_method)(
+          [](const crow::request& req) { return req.body; });
+  CROW_ROUTE(app, "/index/")
+      .methods("POST"_method)(
+          [](const crow::request& req) { return req.body; });
+
   app.port(80).multithreaded().run();
   return 0;
 }
--- project/demo/static/js/search.js
+++ project/demo/static/js/search.js
@@ -1,15 +1,34 @@
-$("#mainForm").submit(() => {
-	const config = {
-		url: "",
-		type: "",
-		datatype: "json",
-		data: {
-		},
-		success: (data) => {
-		},
-		error: (err) => { console.log(err); }
-	};
-	$.ajax(config);
+$("#submit").submit(() => {
+  let keyValues = [];
+  $("#keyField")
+    .children("input")
+    .each(() => {
+      keyValues.push($(this).val());
+    });
+  console.log(ek);
+  const config = {
+    url: "/search",
+    type: "POST",
+    datatype: "json",
+    data: {
+      user: $("#username").value(),
+      pass: $("#password").value(),
+      db: $("#database").value(),
+      table: $("#table").value(),
+      key: keyValues,
+    },
+    success: (data) => {
+      console.log(data);
+
+      $("#dashboard").text(data);
+    },
+    error: (err) => {
+      console.log(err);
+    },
+  };
+  $.ajax(config);
 });
 
-$("#addField").click(() => { $("<input type=\"text\" placeholder=\"field\"></>").insertBefore($("#addField")); });
+$("#addField").click(() => {
+  $("#addField").before($("#addField").prev().clone());
+});
